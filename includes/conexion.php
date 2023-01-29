17d5322ab6574274bc96f2d24824faacc00a1a8b
hernansm1983 <110942337+hernansm1983@users.noreply.github.com>
2023-01-28 23:40:11 -0300

    db conexion
    remoto y local

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,9 +1,17 @@
 <?php
 // Conexión
-$servidor = 'localhost';
-$usuario = 'root';
-$password = '';
-$basededatos = 'blog_master';
+
+if($_SERVER['SERVER_NAME'] == 'localhost'){
+    $servidor = 'localhost';
+    $usuario = 'root';
+    $password = '';
+    $basededatos = 'blog_master';
+}else{
+    $servidor = 'pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    $usuario = 'elplxav63ajzx6qb';
+    $password = 'evhfzqu3ulqojq5w';
+    $basededatos = 'mnfa0m8qzd4umebz';
+}
 $db = mysqli_connect($servidor, $usuario, $password, $basededatos);
 
 mysqli_query($db, "SET NAMES 'utf8'");
